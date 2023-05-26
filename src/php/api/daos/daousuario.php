48babdc8d20b78e63604b541e2b152c88f827d95
David Pérez <117274355+dperezsache@users.noreply.github.com>
2023-05-26 16:42:28 +0200

    Sprint4 david (#21)
    * Mockup registro de padres
    
    * Sprint 1
    
    * Sprint 1 mejoras
    
    * Sprint 1 David
    
    * Alta y mod. padres actualizados
    
    * SQL correción
    
    * SQL correción
    
    * SQL correción
    
    * SQL correción
    
    * Correciones modificación padres
    
    * Sprint 1 fin
    
    * Arquitectura nueva (falta login Google)
    
    * Arquitectura nueva (falta login Google)
    
    * Arquitectura nueva (falta login Google)
    
    * Login Google más o menos
    
    * Login Google más o menos
    
    * Inicios sesión Google
    
    * Inicios sesión Google
    
    * Alta de hijos operativo
    
    * Sprint 2
    
    * Sprint 2
    
    * Menú hamburguesa xd
    
    * Recuperación contraseñas
    
    * Recuperación contraseñas
    
    * Recuperación contraseñas
    
    * Fixes varios
    
    * Fix nombre archivos SQL
    
    * Fix nombre archivos SQL
    
    * No lo sé
    
    * Redirección con Google y gestión error
    
    * arreglada cositas y en proceso crud de hijos
    
    * Listado y eliminado de hijos
    
    * crud funcionando, script no finalizado
    
    * Arreglos gestión hijos
    
    * nuevo script, arreglado el daousuario a la espera de cambios en cursos
    
    * Gestión hijos fixes
    
    * Calendario hijos comedor funcionando
    
    * Comprobaciones días comedor
    
    * :bug: arreglado
    
    * :construction: marcar/desmarcar mes entero v1
    
    * Preparar lo de la contraseña pa hosting
    
    * Corregir tabla persona
    
    * Quitar unique al IBAN
    
    * Borrar form al añadir hijo
    
    * 'contrasenia' => 'clave' en todos lados
    
    * Autorización REST
    
    * get cursos en vez de post
    
    * Check de festivos en alta de menús
    
    * Fixes varios
    
    * Desactivar botones al realizar procesos
    
    * Datos de prueba sql
    
    * Update JSDoc
    
    * Menú secretaria y vistas
    
    * Asignación de roles
    
    * Fix roles
    
    ---------
    
    Co-authored-by: SergioSRS <sergioriverasalgado15@gmail.com>

--- src/php/api/daos/daousuario.php
+++ src/php/api/daos/daousuario.php
@@ -471,7 +471,7 @@
                 $usuario->dni = $resultSet[0]['dni'];
                 $usuario->iban = $resultSet[0]['iban'];
                 $usuario->titular = $resultSet[0]['titular'];
-                $usuario->rol = $resultSet[0]['rol'];
+                $usuario->rol = null;
             }
             else {
                 $usuario = false;

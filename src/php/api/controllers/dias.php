71177dd420d217e3648800c9cdf5302df1df5e77
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2024-04-14 17:13:54 +0200

    Comprobacion en parte servidor modificar dias anteriores

--- src/php/api/controllers/dias.php
+++ src/php/api/controllers/dias.php
@@ -46,24 +46,36 @@
                 header('HTTP/1.1 401 Unauthorized');
                 die();
             }
-						//Es fin de semana
-						$diaSemana = date('w', strtotime($datos->dia));
-						if($diaSemana == 0 || $diaSemana == 6){
-							//Abortamos sin error
-            	header('HTTP/1.1 200 OK');
-            	die();
-						}
-						//¿Es festivo
-						if(DAOFestivos::esFestivo($datos->dia)){
-							//Abortamos sin error
-            	header('HTTP/1.1 200 OK');
-            	die();
-						}
-
+        
+            // Verificar si la fecha enviada es menor que la fecha actual
+            $fechaActual = date('Y-m-d');
+            if ($datos->dia < $fechaActual) {
+                header('HTTP/1.1 400 Bad Request');
+                echo json_encode(array("error" => "La fecha enviada es menor que la fecha actual"));
+                die();
+            }
+        
+            // Es fin de semana
+            $diaSemana = date('w', strtotime($datos->dia));
+            if ($diaSemana == 0 || $diaSemana == 6) {
+                // Abortamos sin error
+                header('HTTP/1.1 200 OK');
+                die();
+            }
+        
+            // ¿Es festivo?
+            if (DAOFestivos::esFestivo($datos->dia)) {
+                // Abortamos sin error
+                header('HTTP/1.1 200 OK');
+                die();
+            }
+        
+            // Si todo está correcto, procedemos a insertar el día
             DAOUsuario::altaDia($datos);
             header('HTTP/1.1 200 OK');
             die();
         }
+        
 
         /**
          * Borrar fila de la tabla dias
@@ -77,12 +89,21 @@
                 header('HTTP/1.1 401 Unauthorized');
                 die();
             }
-
+        
             if (count($pathParams)) {
                 $fecha = new DateTime($pathParams[0]);
-                $fecha = $fecha->format('Y/m/d');
+                $fecha = $fecha->format('Y-m-d');
+        
+                // Verificar si la fecha enviada es menor que la fecha actual
+                $fechaActual = date('Y-m-d');
+                if ($fecha < $fechaActual) {
+                    header('HTTP/1.1 400 Bad Request');
+                    echo json_encode(array("error" => "La fecha enviada es menor que la fecha actual"));
+                    die();
+                }
+        
                 DAOUsuario::eliminarDia($fecha, $pathParams[1], $pathParams[2]);
-
+        
                 header('HTTP/1.1 200 OK');
                 die();
             }
@@ -91,5 +112,6 @@
                 die();
             }
         }
+        
     }
 ?>
